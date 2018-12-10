f7df10c8e4c49f46b09e47e7d36c75bdea723167
Emroze <emrozesd@users.noreply.github.com>
2018-12-10 15:36:34 -0800

    Game start functions added

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,4 +5,39 @@
 // If player goes over score losses++
 // Game resets with new randomly generated numbers
 // Total score set back to zero 
+var gameRandomNumber;
+var totalScore; 
+var wins = 0;
+var losses = 0;
 
+var crystalList = [
+    "assets/images/green_rupee.jpg",
+    "assets/images/blue_rupee.jpg",
+    "assets/images/red_rupee.jpg",
+    "assets/images/yellow_rupee.jpg"
+]; 
+
+function gameStart() {
+
+    gameRandomNumber = Math.floor((Math.random() * 120) + 19); 
+    console.log(gameRandomNumber);
+
+    $("#game-number").text(gameRandomNumber);
+
+    setCrystals(); 
+
+}
+
+function setCrystals() {
+
+    for (var i = 0; i < crystalList.length; i++) {
+        var crystalImages = $("<img>");
+        crystalImages.addClass("crystal-images");
+        crystalImages.attr("src", crystalList[i]);
+        crystalImages.attr("value", (Math.floor(Math.random() * 12) + 1)); 
+        $(".crystal-button").append(crystalImages);
+    }
+
+}
+
+gameStart(); 
