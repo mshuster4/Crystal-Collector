06f5b6a1101ebb8639a552c48b5d4f4cfd1cbdf2
Emroze <emrozesd@users.noreply.github.com>
2018-12-12 20:55:45 -0800

    added style

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,10 +1,4 @@
-// Generate random number between 19-120 
-// Each Crystal represents a hidden random number between 1-12
-// Each time a Crystal is clicked, that number is added to user score total
-// When player matches their score to the randomly generated score, wins ++
-// If player goes over score losses++
-// Game resets with new randomly generated numbers
-// Total score set back to zero 
+
 var gameRandomNumber; 
 var totalScore = 0;
 var wins = 0; 
